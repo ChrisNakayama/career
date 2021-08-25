3a5b15d0d0fa3b0479fe60e831bb6825c5f14d1a
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-08-25 01:39:57 -0700

    redux of project

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,71 @@
-
-
-// When clicking the "Click here to get started!" button, this hides the initial intro page and takes you to the quiz.
 $(document).ready(function() {
-  $("form#intro").submit(function(event) {
-    event.preventDefault();
-    $(".intro").hide();
-    $(".quiz").fadeIn();
-  });
-// If you want to go back to the quiz page, the "Try again" button hides the result and brings up the quiz again.
-  $("form#tryagain").submit(function(event) {
-    event.preventDefault();
-    $(".quiz").fadeIn();
-    $("#C, #Ruby, #python, ").hide();
-  });
-// Each question has three answers with an independent point value of 1-3 for each of the answers.
-// After, we add the amounts, hide the quiz, then display the character based on the result.
-// A "Try again" button is added to redo the quiz and hide the result, see notes on ln 13.
-  $("form#quiz").submit(function(event) {
-    event.preventDefault();
-    let answer1 = parseInt($("input:radio[name=question1]:checked").val());
-    let answer2 = parseInt($("input:radio[name=question2]:checked").val());
-    let answer3 = parseInt($("input:radio[name=question3]:checked").val());
-    let answer4 = parseInt($("input:radio[name=question4]:checked").val());
-    let answer1 = parseInt($("input:radio[name=question5]:checked").val());
-    let result = add(answer1, answer2, answer3, answer4, answer5);
+  $("form.formOne").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var dobInput = new Date($("#born").val());
+    var cancerInput = $("input:radio[name=cancer]:checked").val();
+    var energyColorInput = $("#color").val();
+    var crimeInput = $("#crime").val();
 
-    if (result <= 5) {
-      $("#C").show();
-    } else if (result > 3 && result <= 10) {
-      $("#Ruby").show();
-    } else if (result > 10) {
-      $("#Python").show();
-    }
-    $(".quiz").hide();
-    $(document).ready(function() {
-      
-const age = parseInt(prompt("Are you old enough to code? Are you over 7?"));
-    if (age >= 7) {
-    $('#the-language').show();
+    var score = 0;
+
+ 
+
+    $(".name").text(nameInput);
+    var crimeInputValue = parseInt(crimeInput);
+    var millenial = dobInput.getFullYear();
+    var r = nameInput.includes("r");
+
+    console.log("Name: " + nameInput); 
+    console.log("Year born: " + millenial); 
+    console.log("Energy color: " + energyColorInput); 
+    console.log("Crime score: " + crimeInputValue); 
+    console.log("Score: " + score); 
+
+
+    //$(".formOne").hide();
+    //$(".intro").hide();
+
+    if (r === true) {
+      score += 20;
     } else {
-    $('#not-old').show();
-    }
-    });
+      score += 5;
+    };
+
+    if (millenial >= 1980) {
+      score += 15;
+    } else {
+      score += 10;
+    };
+
+    if (cancerInput === "yes") {
+      score += 5;
+    } else {
+      score += 25;
+    };
+
+    if (energyColorInput.includes("000000") === true) {
+      score += 50;
+    } else {
+      score += 10;
+    };
+
+    if (crimeInputValue <= 3) {
+      score += 15;
+    } else {
+      score += 30;
+    };
+
+    if (score <= 90) {
+      $("#python").show();
+    } else if (score >= 100){
+      $("#kotlin").show();
+    } else {
+      $("#php").show();
+    };
+
+    $(".main").hide();
 
 
+    event.preventDefault();
   });
 });
